9c002b6e63cfde96a5edd89dee8625365dd85163
Peter C <peter.chau.esq@gmail.com>
2016-05-03 08:48:39 +0000

    extracted functions

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 "use strict";
 
+// require npm packages
 const bl = require('bl');
-const fs = require('fs');
 const https = require('https');
-const json2csv = require('json2csv');
 
+// require modules
+const exportAsCsv = require('./exportAsCsv.js');
+
+// set up variables
+// DAO Ethereum address
 const address = '0xbb9bc244d798123fde783fcc1c72d3bb8c189413';
 const date = new Date();
-const file = `output/${date}.csv`;
+// path/to/csv
+const outputPath = `output/${date}.csv`;
+// csv field headers
 const fields = ['blockNumber', 'timeStamp', 'hash', 'nonce', 'blockHash', 'transactionIndex', 'from', 'to', 'value', 'gas', 'gasPrice', 'input', 'contractAddress', 'cumulativeGasUsed', 'gasUsed', 'confirmations'];
 
 process.stdout.write('DAO-datascraper initialized!\n');
 
 // get data from the etherscan.io api https://etherscan.io/apis
+// Make https request
 https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`, (res) => {
 
     process.stdout.write(`StatusCode: ${res.statusCode}\n`);
 
+    // return a String not an Buffer
     res.setEncoding('utf8');
-
+    // wait for and combine all responses
     res.pipe(bl((err, data) => {
 
         if (err) {
@@ -27,28 +35,11 @@ https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${a
 
         process.stdout.write('Getting DAO transaction logs from etherscan\n');
 
+        // convert to JSON
         const json = JSON.parse(data);
 
-        json2csv({
-            data: json["result"],
-            fields: fields
-        }, (err, csv) => {
-
-            if (err) {
-                process.stdout.write(`Error: ${err}\n`);
-            }
-
-            fs.writeFile(file, csv, (err) => {
-
-                if (err) {
-                    process.stdout.write(`Error: ${err}\n`);
-                }
-
-                process.stdout.write(`File saved: ${file}\n`);
-
-            });
-
-        });
+        // convert to csv
+        exportAsCsv(json, fields, outputPath);
 
     }));
 
