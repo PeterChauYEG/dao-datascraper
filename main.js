bd5a197b13298278094dc856b88a3abb85aee3a3
Peter C <peter.chau.esq@gmail.com>
2016-05-05 04:29:11 +0000

    abstracted address

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const https = require('https');
 
 // require modules
 const exportAsCsv = require('./exportAsCsv.js');
+const address = require('./settings.js').address;
 
 // set up variables
 // DAO Ethereum address
-const address = '0xbb9bc244d798123fde783fcc1c72d3bb8c189413';
 const date = new Date();
 // path/to/csv
 const outputPath = `output/${date}.csv`;
@@ -20,31 +20,37 @@ process.stdout.write('DAO-datascraper initialized!\n');
 
 // get data from the etherscan.io api https://etherscan.io/apis
 // Make https request
-https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`, (res) => {
+const server = () => {
+    https.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=asc`, (res) => {
 
-    process.stdout.write(`StatusCode: ${res.statusCode}\n`);
+        process.stdout.write(`StatusCode: ${res.statusCode}\n`);
 
-    // return a String not an Buffer
-    res.setEncoding('utf8');
-    // wait for and combine all responses
-    res.pipe(bl((err, data) => {
+        // return a String not an Buffer
+        res.setEncoding('utf8');
+        // wait for and combine all responses
+        res.pipe(bl((err, data) => {
 
-        if (err) {
-            process.stdout.write(`Error: ${err}\n`);
-        }
+            if (err) {
+                process.stdout.write(`Error: ${err}\n`);
+            }
 
-        process.stdout.write('Getting DAO transaction logs from etherscan\n');
+            process.stdout.write('Getting DAO transaction logs from etherscan\n');
 
-        // convert to JSON
-        const json = JSON.parse(data);
+            // convert to JSON
+            const json = JSON.parse(data);
 
-        // convert to csv
-        exportAsCsv(json, fields, outputPath);
+            // convert to csv
+            exportAsCsv(json, fields, outputPath);
 
-    }));
+        }));
 
-}).on('error', (err) => {
+    }).on('error', (err) => {
 
-    process.stdout.write(`Error: ${err}\n`);
+        process.stdout.write(`Error: ${err}\n`);
 
-});
+    });
+};
+
+server();
+
+module.exports = server;
