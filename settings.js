37077b1634c35cb136cc4be3c297abacd5ef585c
Vid Rowan <vid.rowan@tbis.io>
2018-03-26 12:01:10 -0700

    added other available API options to settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,10 @@
 module.exports = {
     address: '0xbb9bc244d798123fde783fcc1c72d3bb8c189413' //TheDao See: https://etherscan.io/address/0xbb9bc244d798123fde783fcc1c72d3bb8c189413
-    , sortOrder: 'desc' //'asc'
+    , transactionType: 'normal' //'internal'
+    , startblock: '' //ex: 0; blank for default
+    , endblock: '' //ex 99999999; ; blank for default
+    , page: '' //specific page to pull; ex: 1; blank for default
+    , offset: '' //starting tx offset; ex: 10; blank for default
+    , sortOrder: 'asc' //'desc|asc'
+    , apikey: ''; //YourApiKeyToken //blank for none
 };
